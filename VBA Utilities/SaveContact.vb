6ea94c38fe0f8b072e6471c335367fd53c9e8501
Bryan Jenks <bryanjenks@protonmail.com>
2021-04-06 10:43:21 -0700

    formatting
    resolves #1

--- VBA Utilities/SaveContact.vb
+++ VBA Utilities/SaveContact.vb
@@ -1,14 +1,14 @@
 Option Explicit
 Sub ExportOutlookAddressBook()
 
-Dim olApp As Outlook.Application
-Set olApp = Outlook.Application
-    Dim olNS As Outlook.NameSpace
-    Set olNS = olApp.GetNamespace("MAPI")
-        Dim olAL As Outlook.AddressList
-        Set olAL = olNS.AddressLists("Contacts") 'Change name if different contacts list name
-Dim olEntry As Outlook.AddressEntry
-Dim contact 'The contact to extract
+    Dim olApp As Outlook.Application
+    Set olApp = Outlook.Application
+        Dim olNS As Outlook.NameSpace
+        Set olNS = olApp.GetNamespace("MAPI")
+            Dim olAL As Outlook.AddressList
+            Set olAL = olNS.AddressLists("Contacts") 'Change name if different contacts list name
+    Dim olEntry As Outlook.AddressEntry
+    Dim contact 'The contact to extract
 
     Dim vaultPathToSaveFileTo As String
     Dim personNameStartChar As String
@@ -47,75 +47,74 @@ Dim contact 'The contact to extract
      Dim resultString As String
         
 
-        resultString = ""
-        resultString = resultString & "# [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]]"
-        resultString = resultString & vbCrLf & vbCrLf & vbCrLf
-        
-        resultString = resultString & "- `Type:` " & contactTypeLink & vbCrLf
-        resultString = resultString & vbCrLf
-        
-        resultString = resultString & "- `Keywords:` " & vbCrLf
-        resultString = resultString & vbCrLf
-        
-        resultString = resultString & "- `Classification:` " & vbCrLf
-        resultString = resultString & vbCrLf
-        
-        resultString = resultString & "- `Team:` "
-        resultString = resultString & vbCrLf
-        
-        resultString = resultString & "- `Email:` "
-        resultString = resultString & "[" & contact.FirstName & " " & contact.LastName & "]"
-        resultString = resultString & "(mailto:" & contact.IMAddress & ")"
-        resultString = resultString & vbCrLf
-        
-        resultString = resultString & "- `Birthday:` "
-        resultString = resultString & vbCrLf
-        
-        resultString = resultString & "- `Datapoints:` "
-        resultString = resultString & vbCrLf & vbCrLf & vbCrLf
+    resultString = ""
+    resultString = resultString & "# [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]]"
+    resultString = resultString & vbCrLf & vbCrLf & vbCrLf
 
-        
-        resultString = resultString & "---"
-        resultString = resultString & vbCrLf & vbCrLf & vbCrLf
-        
-        
-        resultString = resultString & "### Meetings" & vbCrLf & vbCrLf
-        resultString = resultString & "```dataview" & vbCrLf
-        resultString = resultString & "table file.ctime.year as CY, file.ctime.month as CM, file.ctime.day as CD, file.mtime.year as MY, file.mtime.month as MM, file.mtime.day as MD" & vbCrLf
-        resultString = resultString & "from [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]] and #Meetings" & vbCrLf
-        resultString = resultString & "limit 10" & vbCrLf
-        resultString = resultString & "sort file.ctime desc" & vbCrLf
-        resultString = resultString & "```" & vbCrLf & vbCrLf & vbCrLf
-        
-        
-        resultString = resultString & "### Training" & vbCrLf & vbCrLf
-        resultString = resultString & "```dataview" & vbCrLf
-        resultString = resultString & "table file.ctime.year as CY, file.ctime.month as CM, file.ctime.day as CD, file.mtime.year as MY, file.mtime.month as MM, file.mtime.day as MD" & vbCrLf
-        resultString = resultString & "from [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]] and #Training" & vbCrLf
-        resultString = resultString & "limit 10" & vbCrLf
-        resultString = resultString & "sort file.ctime desc" & vbCrLf
-        resultString = resultString & "```" & vbCrLf & vbCrLf & vbCrLf
-        
-        
-        resultString = resultString & "### Emails" & vbCrLf & vbCrLf
-        resultString = resultString & "```dataview" & vbCrLf
-        resultString = resultString & "table file.ctime.year as CY, file.ctime.month as CM, file.ctime.day as CD, file.mtime.year as MY, file.mtime.month as MM, file.mtime.day as MD" & vbCrLf
-        resultString = resultString & "from [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]] and #Email" & vbCrLf
-        resultString = resultString & "limit 10" & vbCrLf
-        resultString = resultString & "sort file.ctime desc" & vbCrLf
-        resultString = resultString & "```" & vbCrLf & vbCrLf & vbCrLf
-        
-        
-        resultString = resultString & "### Mentions" & vbCrLf & vbCrLf
-        resultString = resultString & "```dataview" & vbCrLf
-        resultString = resultString & "table file.ctime.year as CY, file.ctime.month as CM, file.ctime.day as CD, file.mtime.year as MY, file.mtime.month as MM, file.mtime.day as MD" & vbCrLf
-        resultString = resultString & "from [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]] and !#Email and !#Meetings and !#Training" & vbCrLf
-        resultString = resultString & "limit 10" & vbCrLf
-        resultString = resultString & "sort file.ctime desc" & vbCrLf
-        resultString = resultString & "```" & vbCrLf & vbCrLf & vbCrLf
+    resultString = resultString & "- `Type:` " & contactTypeLink & vbCrLf
+    resultString = resultString & vbCrLf
 
-        outputItem.Body = resultString
+    resultString = resultString & "- `Keywords:` " & vbCrLf
+    resultString = resultString & vbCrLf
 
+    resultString = resultString & "- `Classification:` " & vbCrLf
+    resultString = resultString & vbCrLf
+
+    resultString = resultString & "- `Team:` "
+    resultString = resultString & vbCrLf
+
+    resultString = resultString & "- `Email:` "
+    resultString = resultString & "[" & contact.FirstName & " " & contact.LastName & "]"
+    resultString = resultString & "(mailto:" & contact.IMAddress & ")"
+    resultString = resultString & vbCrLf
+
+    resultString = resultString & "- `Birthday:` "
+    resultString = resultString & vbCrLf
+
+    resultString = resultString & "- `Datapoints:` "
+    resultString = resultString & vbCrLf & vbCrLf & vbCrLf
+
+
+    resultString = resultString & "---"
+    resultString = resultString & vbCrLf & vbCrLf & vbCrLf
+
+
+    resultString = resultString & "### Meetings" & vbCrLf & vbCrLf
+    resultString = resultString & "```dataview" & vbCrLf
+    resultString = resultString & "table file.ctime.year as CY, file.ctime.month as CM, file.ctime.day as CD, file.mtime.year as MY, file.mtime.month as MM, file.mtime.day as MD" & vbCrLf
+    resultString = resultString & "from [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]] and #Meetings" & vbCrLf
+    resultString = resultString & "limit 10" & vbCrLf
+    resultString = resultString & "sort file.ctime desc" & vbCrLf
+    resultString = resultString & "```" & vbCrLf & vbCrLf & vbCrLf
+
+
+    resultString = resultString & "### Training" & vbCrLf & vbCrLf
+    resultString = resultString & "```dataview" & vbCrLf
+    resultString = resultString & "table file.ctime.year as CY, file.ctime.month as CM, file.ctime.day as CD, file.mtime.year as MY, file.mtime.month as MM, file.mtime.day as MD" & vbCrLf
+    resultString = resultString & "from [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]] and #Training" & vbCrLf
+    resultString = resultString & "limit 10" & vbCrLf
+    resultString = resultString & "sort file.ctime desc" & vbCrLf
+    resultString = resultString & "```" & vbCrLf & vbCrLf & vbCrLf
+
+
+    resultString = resultString & "### Emails" & vbCrLf & vbCrLf
+    resultString = resultString & "```dataview" & vbCrLf
+    resultString = resultString & "table file.ctime.year as CY, file.ctime.month as CM, file.ctime.day as CD, file.mtime.year as MY, file.mtime.month as MM, file.mtime.day as MD" & vbCrLf
+    resultString = resultString & "from [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]] and #Email" & vbCrLf
+    resultString = resultString & "limit 10" & vbCrLf
+    resultString = resultString & "sort file.ctime desc" & vbCrLf
+    resultString = resultString & "```" & vbCrLf & vbCrLf & vbCrLf
+
+
+    resultString = resultString & "### Mentions" & vbCrLf & vbCrLf
+    resultString = resultString & "```dataview" & vbCrLf
+    resultString = resultString & "table file.ctime.year as CY, file.ctime.month as CM, file.ctime.day as CD, file.mtime.year as MY, file.mtime.month as MM, file.mtime.day as MD" & vbCrLf
+    resultString = resultString & "from [[" & contactFileNameStartChr & contact.FirstName & " " & contact.LastName & "]] and !#Email and !#Meetings and !#Training" & vbCrLf
+    resultString = resultString & "limit 10" & vbCrLf
+    resultString = resultString & "sort file.ctime desc" & vbCrLf
+    resultString = resultString & "```" & vbCrLf & vbCrLf & vbCrLf
+
+    outputItem.Body = resultString
 
     Dim fileName As String
     ' Now we create the file name
@@ -126,8 +125,8 @@ Dim contact 'The contact to extract
     ' Save the result
     outputItem.SaveAs vaultPathToSaveFileTo & fileName, OLTXT
      
-Set olApp = Nothing
-Set olNS = Nothing
-Set olAL = Nothing
+    Set olApp = Nothing
+    Set olNS = Nothing
+    Set olAL = Nothing
 
 End Sub
